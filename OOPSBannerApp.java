27f5092e2f13affbe81e481359d644f6809c47f8
Shivansh Chugh <shivigames123@gmail.com>
2026-03-12 17:13:20 +0530

    UC6: Introduce helper methods for O, P, S banner patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,49 @@
 public class OOPSBannerApp {
 
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    public static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+    }
+
     public static void main(String[] args) {
 
-        String[] banner = new String[] {
-            String.join("", " ***** ", "   ***** ", "   ***** ", "   ***** "),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", "*     *", " *     *", " *     *", " *     *"),
-            String.join("", "*     *", " ***** ", "   ***** ", "   ***** "),
-            String.join("", "*     *", " *     ", "       *", "       *"),
-            String.join("", "*     *", " *     ", "       *", "       *"),
-            String.join("", " ***** ", " *     ", " ***** ", "   ***** ")
-        };
+        String[] o = getO();
+        String[] p = getP();
+        String[] s = getS();
 
-        for (String line : banner) {
-            System.out.println(line);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(o[i] + " " + o[i] + " " + p[i] + " " + s[i]);
         }
     }
 }
